9c23eb51e430e2278024746cecec47cf293e779e
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2017-04-08 11:04:45 -0700

    Stripped out unused stuff. Added SCSS compilation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import 'font-awesome/css/font-awesome.css';
+import './css/base/style.css';
 
 ReactDOM.render(
   <App />,
