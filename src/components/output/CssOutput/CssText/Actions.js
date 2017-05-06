b852559c7548562644e945abbd6d1df799e6e501
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2017-05-06 14:49:39 -0700

    Began hooking up action button events

--- src/components/output/CssOutput/CssText/Actions.js
+++ src/components/output/CssOutput/CssText/Actions.js
@@ -4,9 +4,9 @@ class Actions extends Component {
   render() {
     return (
         <nav className="actions">
-            <i className="fa fa-cloud-download" title="Download"></i>
-            <i className="fa fa-share" title="Share"></i>
-            <i className="fa fa-copy" title="Copy"></i>
+            <i className="fa fa-cloud-download" title="Download" onClick={this.props.handleDownload}></i>
+            <i className="fa fa-share" title="Share" onClick={this.props.handleShare}></i>
+            <i className="fa fa-copy" title="Copy" onClick={this.props.handleCopy}></i>
         </nav>
     );
   }
--- src/components/output/CssOutput/CssText/Index.js
+++ src/components/output/CssOutput/CssText/Index.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Actions from './Actions';
 
-const CssText = (props) => {
-  return (
-      <section className="css-output ui-column">
-        <header className="ui-column-header">
-          <Actions/>
-          <h2>{props.preprocessor} Output</h2>
-        </header>
-        <pre>
-          <code>{props.children}</code>
-        </pre>
-      </section>
-  );
+class CssText extends Component {
+  download(){
+    console.log("download");
+  }
+  share(){
+    console.log("share");
+  }
+  copy(){
+    console.log("copy");
+  }
+  render(){
+    return (
+        <section className="css-output ui-column">
+          <header className="ui-column-header">
+            <Actions
+              handleDownload={this.download}
+              handleShare={this.share}
+              handleCopy={this.copy}
+            />
+            <h2>{this.props.preprocessor} Output</h2>
+          </header>
+          <pre>
+            <code>{this.props.children}</code>
+          </pre>
+        </section>
+    );
+  }
 }
 
 export default CssText;
