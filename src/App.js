37f36e9e99b6d9d34afa8a9d032f2ba4e1bf3848
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2017-05-13 15:52:55 -0700

    Allow setting of state variables via query string

--- src/App.js
+++ src/App.js
@@ -4,28 +4,45 @@ import Footer from './components/layout/Footer.js';
 import GridForm from './components/forms/GridForm.js';
 import Output from './components/output/Output.js';
 
+const queryString = require('query-string');
+
+var queryState = queryString.parse(location.search);
+
+
+function queryOrDefault(name, defaultValue){
+  if(typeof queryState[name] === "undefined"){
+    return defaultValue;
+  } else{
+    if(queryState[name] === "percent"){
+      return "%";
+    } else{
+      return queryState[name];
+    }
+  }
+}
+
 class App extends Component {
   constructor(){
     super();
 
     this.state = {
       form :{
-        preprocessor: "CSS",
-        columns: 12,
+        preprocessor: queryOrDefault("preprocessor","CSS"),
+        columns: queryOrDefault("columns",12),
         rowMargin: {
-          value:10,
-          unit:"px"
+          value:queryOrDefault("rowMargin.value",10),
+          unit:queryOrDefault("rowMargin.unit","px")
         },
         gutter: {
-          value:1,
-          unit:"%"
+          value:queryOrDefault("gutter.value",1),
+          unit:queryOrDefault("gutter.unit","%")
         },
         indent: {
-          type:"space",
-          number:4
+          type:queryOrDefault("indent.type","space"),
+          number:queryOrDefault("indent.number",4)
         },
-        includeComments: true,
-        minify: false
+        includeComments: queryOrDefault("includeComments",true),
+        minify: queryOrDefault("minify",false)
       }
     }
   }
