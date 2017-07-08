6079b74234f0fc777c4bb114296d3bb0a2fce2b4
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2017-07-08 15:07:42 -0700

    Changed working title

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = (props) => {
   return (
-      <header className="header">Header</header>
+      <header className="header">Robo Grids</header>
   );
 }
 
